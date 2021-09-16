84d6e7a4130595927ad2177662f9586a14ad803e
JamesCasella <james.casella@student.curtin.edu.au>
2021-09-16 10:38:54 +0800

    Adjusted to my format. Added '.gitignore'.

--- src/main/java/ComparisonResult.java
+++ src/main/java/ComparisonResult.java
@@ -2,18 +2,26 @@ package sillypackagenamepleasechange;
 
 public class ComparisonResult
 {
+
+    // - Variables - //
+
     private final String file1;
     private final String file2;
     private final double similarity;
+
+    // - Constructor - //
     
-    public ComparisonResult(String file1, String file2, double similarity)
+    public ComparisonResult(String inFile1, String inFile2, double inSimilarity)
     {
-        this.file1 = file1;
-        this.file2 = file2;
-        this.similarity = similarity;
+        this.file1 = inFile1;
+        this.file2 = inFile2;
+        this.similarity = inSimilarity;
     }
+
+    // - Accessors - //
     
     public String getFile1() { return file1; }
     public String getFile2() { return file2; }
     public double getSimilarity() { return similarity; }
+
 }
--- src/main/java/SillyClassNamePleaseChange.java
+++ src/main/java/SillyClassNamePleaseChange.java
@@ -13,59 +13,54 @@ import java.util.*;
 
 public class SillyClassNamePleaseChange extends Application
 {
+
+    // Launch application
     public static void main(String[] args)
     {
         Application.launch(args);
     }
     
+    // ------------- //
+    // - Variables - //
+    // ------------- //
+
     private TableView<ComparisonResult> resultTable = new TableView<>();  
     private ProgressBar progressBar = new ProgressBar();
     
+    // -------- //
+    // - Main - //
+    // -------- //
+
     @Override
     public void start(Stage stage)
     {
+
+        // - Layout - //
+
+        // Title, min width
         stage.setTitle("SillyWindowTitlePleaseChange");
         stage.setMinWidth(600);
 
-        // Create toolbar
-        Button compareBtn = new Button("Compare...");
-        Button stopBtn = new Button("Stop");
-        ToolBar toolBar = new ToolBar(compareBtn, stopBtn);
-        
-        // Set up button event handlers.
-        compareBtn.setOnAction(event -> crossCompare(stage));
-        stopBtn.setOnAction(event -> stopComparison());
+        // Toolbar
+        Button compareButton = new Button("Compare");
+        Button stopButton = new Button("Stop");
+        ToolBar toolBar = new ToolBar(compareButton, stopButton);
         
         // Initialise progressbar
         progressBar.setProgress(0.0);
         
+        // Create columns, set widths, add to table
         TableColumn<ComparisonResult,String> file1Col = new TableColumn<>("File 1");
         TableColumn<ComparisonResult,String> file2Col = new TableColumn<>("File 2");
         TableColumn<ComparisonResult,String> similarityCol = new TableColumn<>("Similarity");
-        
-        // The following tell JavaFX how to extract information from a ComparisonResult 
-        // object and put it into the three table columns.
-        file1Col.setCellValueFactory(   
-            (cell) -> new SimpleStringProperty(cell.getValue().getFile1()) );
-            
-        file2Col.setCellValueFactory(   
-            (cell) -> new SimpleStringProperty(cell.getValue().getFile2()) );
-            
-        similarityCol.setCellValueFactory(  
-            (cell) -> new SimpleStringProperty(
-                String.format("%.1f%%", cell.getValue().getSimilarity() * 100.0)) );
-          
-        // Set and adjust table column widths.
         file1Col.prefWidthProperty().bind(resultTable.widthProperty().multiply(0.40));
         file2Col.prefWidthProperty().bind(resultTable.widthProperty().multiply(0.40));
-        similarityCol.prefWidthProperty().bind(resultTable.widthProperty().multiply(0.20));            
-        
-        // Add the columns to the table.
+        similarityCol.prefWidthProperty().bind(resultTable.widthProperty().multiply(0.20));
         resultTable.getColumns().add(file1Col);
         resultTable.getColumns().add(file2Col);
         resultTable.getColumns().add(similarityCol);
 
-        // Add the main parts of the UI to the window.
+        // Add main parts to window
         BorderPane mainBox = new BorderPane();
         mainBox.setTop(toolBar);
         mainBox.setCenter(resultTable);
@@ -74,8 +69,24 @@ public class SillyClassNamePleaseChange extends Application
         stage.setScene(scene);
         stage.sizeToScene();
         stage.show();
+
+        // - Functions - //
+        
+        // Toolbar buttons
+        compareButton.setOnAction(event -> crossCompare(stage));
+        stopButton.setOnAction(event -> stopComparison());
+        
+        // How to extract ComparisonResult and put into the three columns
+        file1Col.setCellValueFactory(      (cell) -> new SimpleStringProperty( cell.getValue().getFile1() ) );
+        file2Col.setCellValueFactory(      (cell) -> new SimpleStringProperty( cell.getValue().getFile2() ) );
+        similarityCol.setCellValueFactory( (cell) -> new SimpleStringProperty( String.format("%.1f%%", cell.getValue().getSimilarity() * 100.0)) );
+
     }
     
+    // ------------------------- //
+    // - BUTTON: Cross Compare - //
+    // ------------------------- //
+
     private void crossCompare(Stage stage)
     {
         DirectoryChooser dc = new DirectoryChooser();
@@ -87,11 +98,23 @@ public class SillyClassNamePleaseChange extends Application
         
         // Extremely fake way of demonstrating how to use the progress bar (noting that it can 
         // actually only be set to one value, from 0-1, at a time.)
-        progressBar.setProgress(0.25);
-        progressBar.setProgress(0.5);
-        progressBar.setProgress(0.6);
-        progressBar.setProgress(0.85);
-        progressBar.setProgress(1.0);
+        try
+        {
+            Thread.sleep(1000);
+            progressBar.setProgress(0.25);
+            Thread.sleep(1000);
+            progressBar.setProgress(0.5);
+            Thread.sleep(1000);
+            progressBar.setProgress(0.6);
+            Thread.sleep(1000);
+            progressBar.setProgress(0.85);
+            Thread.sleep(1000);
+            progressBar.setProgress(1.0);
+        }
+        catch( InterruptedException e )
+        {
+            System.out.println( e.toString() );
+        }
 
         // Extremely fake way of demonstrating how to update the table (noting that this shouldn't
         // just happen once at the end, but progressively as each result is obtained.)
@@ -104,7 +127,11 @@ public class SillyClassNamePleaseChange extends Application
         
         // progressBar.setProgress(0.0); // Reset progress bar after successful comparison?
     }
-    
+
+    // ---------------- //
+    // - BUTTON: Stop - //
+    // ---------------- //
+
     private void stopComparison()
     {
         System.out.println("Stopping comparison...");
